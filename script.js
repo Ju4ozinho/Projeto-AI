65c34df53c07679c4e0502f8b89928759253733d
Maria vitoria <145450863+mavilok14@users.noreply.github.com>
2025-08-21 07:57:03 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -6,66 +6,66 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
+        enunciado: "A dieta dos brasileiros piorou: menos consumo de alimentos naturais, como frutas e vegetais, e mais de ultraprocessados. O consumo de sal e açúcar está acima do recomendado, apesar do arroz e feijão ainda serem a base da alimentação.",
         alternativas: [
             {
                 texto: "Isso é assustador!",
                 afirmacao: "afirmação"
             },
             {
-                texto: "Isso é maravilhoso!",
+                texto: "Isso não me preocupa!",
                 afirmacao: "afirmação"
             }
         ]
     },
     {
-        enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial, uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
+        enunciado: "Uma alimentação saudável fortalece o corpo, a mente e o sistema imunológico, prevenindo doenças e garantindo mais energia e longevidade.",
         alternativas: [
             {
-                texto: "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
+                texto: "Utiliza uma ferramenta de busca na internet para que você fique infortamado do assundo é muito imporante.",
                 afirmacao: "afirmação"
-            },
+            },Escreve
             {
-                texto: "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
+                texto: " Escreva na guia de pesquisa do seu dispositivo é fiquem atentos as preocausões",
                 afirmacao: "afirmação"
             }
         ]
     },
     {
-        enunciado: "Após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
+        enunciado: "Para ter uma alimentação saudável, priorize alimentos naturais como frutas, legumes e carnes magras, enquanto reduz o consumo de ultraprocessados. Beba bastante água, cozinhe mais em casa para controlar os ingredientes e não pule refeições. Comer devagar e buscar a orientação de um nutricionista também são hábitos importantes.",
         alternativas: [
             {
-                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
+                texto: "Praticar exercicio também é bem importante, fazer algum esporte ou até mesmo uma caminhada na rua.",
                 afirmacao: "afirmação"
             },
             {
-                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
+                texto: "Me preocupo com as pessoas que perderão sua qualidade de vida por conta da sua saúde",
                 afirmacao: "afirmação"
             }
         ]
     },
     {
-        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
+        enunciado: "Você pode mudar a sua realiade agora, o seu futuro e o tanto que vocẽ vai evoluir so depende de você",
         alternativas: [
             {
-                texto: "Criar uma imagem utilizando uma plataforma de design como o Paint.",
+                texto: "criar um cronograma com a sua nova rotinha pode te ajudar",
                 afirmacao: "afirmação"
             },
             {
-                texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
+                texto: "crie uma rotina de acordo com a sua realidade, começando de forma facil e evoluindo aos poucos ",
                 afirmacao: "afirmação"
             }
         ]
     },
     {
-        enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda da IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz? ",
+        enunciado: "Em 2049, João... João, aos 35 anos, sentia-se cansado e com problemas de saúde devido à sua alimentação desregrada, baseada em fast-food. Após um alerta médico, ele decidiu mudar, substituindo refrigerantes por água e preparando suas próprias refeições saudáveis. Em três meses, perdeu peso, recuperou a energia e a concentração, descobrindo que a alimentação saudável não só melhorou sua saúde física, mas também seu bem-estar geral.",
         alternativas: [
             {
-                texto: "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
+                texto: "mudar de vida agora.",
                 afirmacao: "afirmação"
             },
             {
-                texto: "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
+                texto: "deixar para depois.",
                 afirmacao: "afirmação"
             }
         ]
